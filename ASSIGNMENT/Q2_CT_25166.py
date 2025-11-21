dabaefa2f333488e4cc2c6e593efebf1be1566d8
Memoona-754 <memoonamehboob754@gmail.com>
2025-11-21 18:45:03 +0500

    Python

--- ASSIGNMENT/Q2_CT_25166.py
+++
@@ -1,2 +0,0 @@
-arr = eval(input())   # input like: [54, 43, 2, 1, 5]
-print(len(arr), end="")
---
+++ ASSIGNMENT/week 11/Q1_CT_25166.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+op = int(input())
+
+if op == 1:
+    print(a + b)
+elif  op == 2:
+    print(a - b)
+elif op == 3:
+    print(a * b)
+else:
+    print("Invalid Input")
